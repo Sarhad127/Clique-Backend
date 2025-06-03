33cb6966a76ce990152e0847da369be28a3cce1b
Sarhad Bahrami <Sarhad94127@hotmail.com>
2025-06-03 18:42:06 +0200

    initial commit

---
+++ src/test/java/org/tutorial/clique/CliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorial.clique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
